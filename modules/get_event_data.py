0abca0182de83e49e2a667f4a12cf23115333aec
godfreykaris <godfreykarisw@gmail.com>
2023-06-22 18:36:55 +0300

    Third Commit

--- modules/get_event_data.py
+++ modules/get_event_data.py
@@ -1,18 +1,9 @@
-from web3 import Web3
 
 class EventDataExtracter:
-    #The OVM endpoint allows us to interact with the contract via web3
-    def __init__(self, ovm_endpoint):
-        # Connect to the Optimism OVM endpoint
-        self.web3 = Web3(Web3.HTTPProvider(ovm_endpoint))
-    
-    def initialize_contract(self, contract_address, contract_abi_file):
-        # Get the contract abi
-        with open(contract_abi_file, 'r') as file:
-            contract_abi = file.read() 
-            
-        # Instantiate the contract
-        self.contract = self.web3.eth.contract(address=contract_address, abi=contract_abi)
+    def __init__(self, web3, contract):
+        self.web3 = web3
+        self.contract = contract
+        
     
     def get_event_logs(self, event_name):
         # Get the event logs for the given event name
@@ -20,8 +11,7 @@ class EventDataExtracter:
 
         event_logs = self.contract.events[event_name]().get_logs(
             {
-                "fromBlock": 0,
-                "toBlock": "latest",
+                "fromBlock": "latest",
                 "topics": [event_signature]
             }
         )
--- modules/store_event_data.py
+++ modules/store_event_data.py
@@ -1,20 +1,34 @@
 import json
-import psycopg2
 
 class EventDataStore:
-    def __init__(self, database_connection):
-        self.database_connection = database_connection
+    def __init__(self, database_object):
+        self.database_object = database_object
     
-    def store_event_data(self, event_logs):
-        with self.connection.cursor() as cursor:
-            for log in event_logs:
-                event_name = log.event
-                event_data = json.dumps(log.args)
+    def store_event_data(self, contract_address, event_name, transaction_hash, event_data):
+        #Get the database connection
+        database_connection = self.database_object.get_database_connection()
+        
+        cursor = database_connection.cursor() 
+        
+        insert_query = "INSERT INTO contract_" + contract_address + "_events (event_name, transaction_hash, event_data) VALUES (%s, %s, %s); "
+        cursor.execute(insert_query, (event_name, transaction_hash, json.dumps(event_data)))
 
-                insert_query = """
-                INSERT INTO contract_events (event_name, event_data)
-                VALUES (%s, %s);
-                """
-                cursor.execute(insert_query, (event_name, event_data))
-
-        self.database_connection.commit()
+        database_connection.commit()
+        cursor.close()
+        database_connection.close()
+        
+    def is_duplicate_event(self, contract_id, event_name, transaction_hash):
+        #Get the database connection
+        database_connection = self.database_object.get_database_connection()
+        
+        cursor = database_connection.cursor() 
+        
+        query = "SELECT EXISTS ( SELECT 1 FROM contract_" + contract_id + "_events WHERE event_name = %s AND transaction_hash = %s);"
+        
+        cursor.execute(query, (event_name, transaction_hash))
+        exists = cursor.fetchone()[0]
+        
+        cursor.close()
+        database_connection.close()
+        
+        return exists
